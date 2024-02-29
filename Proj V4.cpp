325529a36722450dddd8a783b75fa053b13cd9f7
GUSTGUSTS <141816627+GUSTGUSTS@users.noreply.github.com>
2024-02-29 19:09:47 +0700

    Update Proj V4.cpp add gamemode 3 life
    add gamemode 3 life

--- Proj V4.cpp
+++ Proj V4.cpp
@@ -3,6 +3,8 @@
 
 
 
+
+
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -141,11 +143,12 @@ void start() {
     cout << "[2]. science" << endl;
     cout << "[3]. mathematics" << endl;
     cout << "[4]. geography" << endl;
+    cout << "[5]. Gamemode 3 life" << endl;
     do {
         cout << "Choose which topic you want to play: ";
         cin >> topic;
         cin.clear();
-    } while (topic != "1" && topic != "2" && topic != "3"&& topic != "4");
+    } while (topic != "1" && topic != "2" && topic != "3"&& topic != "4"&& topic != "5");
     string response;
     cout << "Are you ready to take the Quiz " << Name[0] << "? (Yes/No): ";
     cin >> response;
@@ -222,7 +225,7 @@ void quiz() {
         {"Which animal is cold-blooded animals?", {"Dog", "Cat", "Dolphin", "salmon"}, 4},
         {"which animal has the most population?", {"Insects", "Fish", "Birds", "Human"}, 1}
     };
-
+   
     int numQuestions = sizeof(questions) / sizeof(questions[0]);
     int score = 0;
     shuffleQuestions(questions, numQuestions);
@@ -263,7 +266,7 @@ void quiz2() {
         {"which planet has the most moons?", {"Saturn", "Neptune", "Mars", "Mercury"}, 1},
         {"which planet closest to the sun?", {"Mars", "jupiter ", "Mercury", "uranus"}, 3}
     };
-
+    
     int numQuestions = sizeof(questions) / sizeof(questions[0]);
     int score = 0;
     shuffleQuestions(questions, numQuestions);
@@ -304,7 +307,7 @@ void quiz3() {
         {"2+2", {"0", "4", "1", "5"}, 2},
         {"321x123", {"39483", "38755", "48653", "32123"}, 1}
     };
-
+     
     int numQuestions = sizeof(questions) / sizeof(questions[0]);
     int score = 0;
     shuffleQuestions(questions, numQuestions);
@@ -365,6 +368,99 @@ void quiz4() {
     // Display final score
     displayScore(score, 10);
 }
+
+void quiz3l() {
+    // Array of questions ภูมิศาสตร์
+    Question questions[] = {
+        {"How many continents are there on Earth?", {"4", "6", "7", "9"}, 3},
+        {"Which is the largest ocean in the world?", {"Indian Ocean", " Pacific Ocean", " Atlantic Ocean", " Arctic Ocean"}, 2},
+        {"Which continent is the largest in size?", {"Asia", " Europe", "North America", " Antarctica"}, 1},
+        {"Which continent has the largest population?",{"Asia", " Europe", "North America", " Antarctica"}, 1},
+        {"How many oceans are there in total?", {"4", "5", "6", "7"}, 3},
+        {"what is capital city of france?", {"Paris", "Rome", "London", "Berlin"}, 1},
+        {"what largest country in the world?", {"Russia", "china", "india", "USA"}, 1},
+        {"what is largest population country in the world?", {"Russia", "china", "india", "USA"}, 3},
+        {"which country is in asia?", {"france", "germany", "russia", "spain"}, 3},
+        {"which continent did Christopher Columbus accidentally found?", {"Asia", "Europe", "america", " Africa"}, 3},
+        {"which continent is Thailand locate?", {"Asia", "Europe", "Australia", " Africa"}, 1},
+        {"which country has same name as a continent?", {"Asia", "Europe", "Australia", " Africa"}, 3},
+        {"which one is the smallest continent?", {"Australia", "North America", "South america", "Asia"}, 1},
+        {"which country mount everest located?", {"Nepal", "china", "india", "Sri Lanka"}, 1},
+        {"what country have hawaii?", {"england", "China", "russia", "USA"}, 4},
+        
+        {"2x + 5 when x = 2", {"4", "6", "7", "9"}, 4},
+        {"√625 + 50 - 20", {"65", "75", "55", "85"}, 3},
+        {"7 x 7 - 3", {"46", "47", "45", "49"}, 1},
+        {"6 + 3 - 4 + 2", {"6", "7", "8", "9"}, 2},
+        {"7 + 6 + 4 + 7", {"21", "23", "24", "26"}, 3},
+        {"378/0", {"0", "378", "1", "undefined"}, 4},
+        {"2x2x2x2/4", {"2", "4", "8", "0"}, 2},
+        {"10persent of 139", {"12.5", "13.9", "10", "13.99"}, 2},
+        {"What is least common multiple of 3 and 12", {"21", "24", "12", "36"}, 3},
+        {"What is greatest common factor of 36 and 4", {"4", "36", "24", "2"}, 1},
+        {"1+1+1+1+1+1+1+1+1+1*2", {"11", "20", "5", "12"}, 1},
+        {"sin 30° equal", {"1", "2", "0", "0.5"}, 4},
+        {"size of rectangle that length=10  width=30 ", {"300", "40", "250", "400"}, 1},
+        {"2+2", {"0", "4", "1", "5"}, 2},
+        {"321x123", {"39483", "38755", "48653", "32123"}, 1},
+        
+         {"What did Isaac Newton discover?", {"X-ray", "Law of Gravitation", "Calculator", "Electricity"}, 2},
+        {"Which mineral is essential for bones?", {"Carbon", "Chlorine", "Calcium", " Sodium"}, 3},
+        {"Where are white blood cells produced?", {"Kidney", " Heart", " Spleen", "Bone marrow"}, 4},
+        {"Earth is the ____ planet in the solar system.", {"2", "3", "4", "5"}, 2},
+        {"The chemical symbol for the element Phosphorus is:", {"F", "Fe", "P", "K"}, 3},
+        {"The chemical symbol for water is:", {"H2O", "CO2", "NO2", "H2O2"}, 1},
+        {"How many colors do rainbow have", {"5", "8", "7", "6"}, 3},
+        {"how many teeth do adults have?", {"34", "32", "30", "28"}, 2},
+        {"Which food group is flour in ?", {"protein", "carbohydrates", "Fat", "vitamin"}, 2},
+        {"How long is 1 rotation of the Earth in hours?", {"12", "18", "30", "24"}, 4},
+        {"which gas do plants absorb for photosynthesis?", {"argon", "Oxygen", "Nitrogen", "Carbon dioxide"}, 4},
+        {"what happen if weight on moon", {"decreases", "increase", "same", "all correct"}, 1},
+        {"water turn to ice at what temperature?", {"50 degree Celsius", "100 degree Celsius", "0 degree Celsius", "150 degree Celsius"}, 3},
+        {"which planet has the most moons?", {"Saturn", "Neptune", "Mars", "Mercury"}, 1},
+        {"which planet closest to the sun?", {"Mars", "jupiter ", "Mercury", "uranus"}, 3},
+        
+        {"Which animal spends 90persent of its day sleeping?", {"Koala bear", "Panda bear", "Polar bear", "Grizzly bear"}, 1},
+        {"Which animal is the fastest land animal?", {"Cheetah", "Leopard", "Tiger", "Ostrich"}, 1},
+        {"Which animal has the longest lifespan in the world?", {"Tortoise", "Blue Whale", "Dolphin", "White shark"}, 1},
+        {"Which animal species is most at risk of extinction?", {"Owl", "Heron", "Kiwi bird", "Hummingbird"}, 3},
+        {"Which animal is highly intelligent with a high IQ?", {"Dog", "Cat", "Dolphin", "Fox"}, 3},
+        {"Which animal is not fish?", {"salmon", "shark", "puffer fish", "whale"}, 4},
+        {"Which animal dose not have lungs?", {"frogs", "snail", "octopus", "elephant"}, 3},
+        {"how many hearts does an octopus have?", {"0", "1", "3", "2"}, 3},
+        {"how many legs dose spider have?", {"4", "6", "8", "10"}, 3},
+        {"Which animal is reptiles?", {"frogs", "Cat", "Dolphin", "snake"}, 4},
+        {"Which animal is Amphibians?", {"salamander", "Cat", "Dolphin", "alligator"}, 1},
+        {"Which animal is Mammals?", {"pigeon", "salamander", "Dolphin", "snake"}, 3},
+        {"Which animal dose not have spine?", {"turtle", "salamander", "Dolphin", "octopus"}, 4},
+        {"Which animal is cold-blooded animals?", {"Dog", "Cat", "Dolphin", "salmon"}, 4},
+        {"which animal has the most population?", {"Insects", "Fish", "Birds", "Human"}, 1}
+    };
+    
+
+    int numQuestions = sizeof(questions) / sizeof(questions[0]);
+    int life = 3;
+    int cquiz = 0;
+    shuffleQuestions(questions, numQuestions);
+    // Iterate through each question
+    for (int i = 0; i < 60; ++i) {
+        cout << "\n************ Question " << i + 1 << " ************" << "       -||" << life << " life remaining||-" << endl;
+        if (askQuestion(questions[i])) {
+            cout << "Correct!" << endl;
+            cquiz++;
+        } else {
+            cout << "Incorrect!" << endl;
+            life--;
+        }
+        if(life == 0){ break;}
+        // Pause for 2 seconds before displaying the next question
+        this_thread::sleep_for(chrono::seconds(2));
+    }
+
+    // Display final score
+    cout << "You answered " << cquiz << " questions correctly";
+}
+
 // Function to display the main menu
 void menu() {
     system("cls");
@@ -390,6 +486,7 @@ void menu() {
 // Main function
 int main() {
     menu();
+  
     switch (choice) {
         case 1:
             start();
@@ -401,6 +498,8 @@ int main() {
                 quiz3();
             } else if(topic == "4") {
                 quiz4();
+            } else if(topic == "5") {
+                quiz3l();
             }
             break;
         case 2:
@@ -425,8 +524,7 @@ int main() {
     }
     
     // After the quiz is completed, display the final score
-    displayScore(score, totalQuestions);
+    if(topic != "5"){displayScore(score, totalQuestions);}
     
     return 0;
 }
-
